109f88ea5b24c8e0dc1c33c20515720f13ab84a9
JamesHenderson627 <jhenderson627@gmail.com>
2015-09-09 18:31:43 -0500

    Adding jsFunctions1-5.js

---
+++ jsFunctions1-5.js
@@ -0,0 +1,95 @@
+// ----------------------------
+// using reduce() from above, write your own sum()
+// that adds up all arguments to sum (note: variadic behavior)
+// ----------------------------
+
+function sum(){
+    // YOUR CODE HERE
+    var inputs = Array.prototype.slice.call(arguments);
+    var result = 0;
+    inputs.forEach(
+        function(el){
+            result += el;
+        }
+    )
+    return result;
+}
+  
+// tests
+// ---
+console.assert(sum(1, 2, 3) === 6)
+console.assert(sum(1, 2, 3, 4) === 10)
+console.assert(sum(1, 2, 3, 4, 5) === 15)
+
+// ----------------------------
+// using Array.sort(), sort the following array
+// of people by name
+// ----------------------------
+
+var people = [
+    {name:"Matt", alma_mater:"Univ of Texas - Austin"},
+    {name:"Brian", alma_mater:"Texas A&M"},
+    {name:"Jesse", alma_mater:"Univ of Texas - Austin"},
+    {name:"Justin", alma_mater:"Univ of Florida"}
+]
+
+people.sort(function(a, b){
+    // YOUR CODE HERE
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    return 0;
+})
+
+// tests
+// ---
+console.assert(people[0].name === "Brian")
+console.assert(people[1].name === "Jesse")
+console.assert(people[2].name === "Justin")
+
+// ----------------------------
+// Using Array.map(), Array.filter(), and Array.sort() on the
+// array below:
+// - filter for customers whose first-names start with 'J',
+// - map to their fullnames,
+// - and then sort the items alphabetically by fullname
+// ----------------------------
+
+var customers = [
+    { first: 'Joe', last: 'Blogs'},
+    { first: 'John', last: 'Smith'},
+    { first: 'Dave', last: 'Jones'},
+    { first: 'Jack', last: 'White'}
+]
+
+var results = customers
+    .filter(function(a){
+        // YOUR CODE HERE
+        if (a.first[0] === "J"){
+            return a;
+        }
+    })
+    .map(function(a){
+        // YOUR CODE HERE
+        var name = {}
+        name["fullname"] = a.first + " " + a.last
+        return name
+    })
+    .sort(function(a, b){
+        // YOUR CODE HERE
+        if (a.fullname > b.fullname) {
+            return 1;
+        }
+        if (a.fullname < b.fullname) {
+            return -1;
+        }
+        return 0;
+        })
+
+// tests
+// ---
+console.assert(results[0].fullname === "Jack White")
+console.assert(results[2].fullname === "John Smith")
